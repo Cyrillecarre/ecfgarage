1a4804275fa2bc8b80aefa84d839bcdbc29b5ed3
Cyrille CARRE <cyrille.carre@gmail.com>
2024-01-04 10:31:44 +0100

    modification_accueil

---
+++ script.js
@@ -0,0 +1,9 @@
+var map = L.map('map').setView([43.6047, 1.4442], 10);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+
+    L.marker([43.6047, 1.4442]).addTo(map)
+      .bindPopup('Garage V.PARROT')
+      .openPopup();
